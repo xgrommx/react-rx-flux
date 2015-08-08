a8f92f14b872337736a5ec67fb953eae2153f4f2
Denis Stoyanov <stoyanov.gr@gmail.com>
2015-08-08 03:48:42 +0300

    According with https://github.com/gaearon/react-redux#dumb-component-is-unaware-of-redux I need separate my component on dumb and smart.

--- src/views/main.js
+++ src/views/main.js
@@ -1,11 +1,159 @@
 import {CompositeDisposable} from 'rx';
 import I, {Map, List} from 'immutable';
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import R from 'ramda';
 import Loader from 'react-loader';
 import Actions from '../actions';
 import Store from '../store';
 import ViewProfile from './profile';
+
+class View extends Component {
+    static propTypes = {
+        person: PropTypes.object.isRequired,
+        loaded: PropTypes.bool.isRequired,
+        friend: PropTypes.string.isRequired,
+        history: PropTypes.object.isRequired,
+        future: PropTypes.object.isRequired,
+        removeFriend: PropTypes.func.isRequired,
+        addFriend: PropTypes.func.isRequired,
+        undo: PropTypes.func.isRequired,
+        redo: PropTypes.func.isRequired,
+        handleChange: PropTypes.func.isRequired,
+        onSave: PropTypes.func.isRequired
+    };
+
+    render() {
+        let {person, loaded, friend, history, future, removeFriend, addFriend, undo, redo, handleChange, onSave} = this.props;
+
+        if(!person || !(person instanceof Map) || loaded) {
+            let options = {
+                lines: 17,
+                length: 20,
+                width: 15,
+                radius: 50,
+                corners: 1,
+                rotate: 0,
+                direction: 1,
+                color: '#000',
+                speed: 1,
+                trail: 60,
+                shadow: false,
+                hwaccel: false,
+                zIndex: 2e9,
+                top: '50%',
+                left: '50%',
+                scale: 1.00
+            };
+
+            return <div className="row"><Loader options={options} /></div>;
+        } else {
+            let friendsView = (friends) => {
+                if(friends.length > 0) {
+                    return (<div className="panel panel-default">
+                        <div className="panel-heading">
+                            <h3 className="panel-title">Your Friends</h3>
+                        </div>
+                        <ul className="list-group">
+                            {
+                                friends.map((friend, i) =>
+                                        <li className="list-group-item" key={i}>
+                                            <span>{friend}</span>
+                                            <button type="button" className="close pull-right" aria-label="Close" onClick={removeFriend.bind(this, i)}><span aria-hidden="true">&times;</span></button>
+                                        </li>
+                                )
+                            }
+                        </ul>
+                        <div className="panel-footer clearfix">
+                            <div className="pull-right">
+                                Friends <span className="badge">{friends.length}</span>
+                            </div>
+                        </div>
+                    </div>);
+                } else {
+                    return (<div>No friends.</div>);
+                }
+            };
+
+            return (<div className="row">
+                <div className="col-md-4 col-xs-4"><ViewProfile person={person} /></div>
+                <div className="col-md-8 col-xs-8">
+                    <form className="form-horizontal">
+                        <div className="form-group">
+                            <div className="col-md-offset-3 col-xs-offset-3 col-md-9 col-xs-9">
+                                <div className="pull-left">
+                                    <button disabled={history.size < 1} className="btn btn-success btn-block" onClick={undo}>Undo</button>
+                                </div>
+                                <div className="pull-right">
+                                    <button disabled={future.size < 1} className="btn btn-success btn-block" onClick={redo}>Redo</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label className="col-md-3 col-xs-3 control-label">First Name:</label>
+                            <div className="col-md-9 col-xs-9">
+                                <input type="text"
+                                       onChange={handleChange}
+                                       name="firstName"
+                                       value={person.get('firstName')}
+                                       className="form-control" placeholder="First Name" />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label className="col-md-3 col-xs-3 control-label">Last Name:</label>
+                            <div className="col-md-9 col-xs-9">
+                                <input type="text"
+                                       onChange={handleChange}
+                                       name="lastName"
+                                       value={person.get('lastName')}
+                                       className="form-control" placeholder="Last Name" />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label className="col-md-3 col-xs-3 control-label">Country:</label>
+                            <div className="col-md-9 col-xs-9">
+                                <input type="text"
+                                       onChange={handleChange}
+                                       name="countryName"
+                                       value={person.get('country').get('name')}
+                                       className="form-control" placeholder="Country" />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label className="col-md-3 col-xs-3 control-label">Friends:</label>
+                            <div className="col-md-9 col-xs-9">
+                                <div className="form-control-static">
+                                    <div className="form-group">
+                                        <div className="col-md-12 col-xs-12">
+                                            <input className="form-control"
+                                                   placeholder="Friend"
+                                                   type="text"
+                                                   onChange={handleChange}
+                                                   value={friend}
+                                                   name="friend"/>
+                                        </div>
+                                    </div>
+                                    <div className="form-group">
+                                        <div className="col-md-9 col-xs-9">
+                                            <button disabled={friend == ""}
+                                                    className="btn btn-success"
+                                                    onClick={addFriend}>Add friend</button>
+                                        </div>
+                                    </div>
+                                    { friendsView(person.get('friends').toArray()) }
+                                </div>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-md-offset-3 col-xs-offset-3 col-md-9 col-xs-9">
+                                <button className="btn btn-info btn-block" onClick={onSave}>Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>);
+        }
+    }
+}
 
 export default class extends Component {
     state = {
@@ -104,132 +252,13 @@ export default class extends Component {
     };
 
     render() {
-        if(!this.state.person || !(this.state.person instanceof Map) || this.state.loaded) {
-            let options = {
-                lines: 17,
-                length: 20,
-                width: 15,
-                radius: 50,
-                corners: 1,
-                rotate: 0,
-                direction: 1,
-                color: '#000',
-                speed: 1,
-                trail: 60,
-                shadow: false,
-                hwaccel: false,
-                zIndex: 2e9,
-                top: '50%',
-                left: '50%',
-                scale: 1.00
-            };
-
-            return <div className="row"><Loader options={options} /></div>;
-        } else {
-            let friendsView = (friends) => {
-                if(friends.length > 0) {
-                    return (<div className="panel panel-default">
-                        <div className="panel-heading">
-                            <h3 className="panel-title">Your Friends</h3>
-                        </div>
-                        <ul className="list-group">
-                            {
-                                friends.map((friend, i) =>
-                                    <li className="list-group-item" key={i}>
-                                        <span>{friend}</span>
-                                        <button type="button" className="close pull-right" aria-label="Close" onClick={this.removeFriend.bind(this, i)}><span aria-hidden="true">&times;</span></button>
-                                    </li>
-                                )
-                            }
-                        </ul>
-                        <div className="panel-footer clearfix">
-                            <div className="pull-right">
-                                Friends <span className="badge">{friends.length}</span>
-                            </div>
-                        </div>
-                    </div>);
-                } else {
-                    return (<div>No friends.</div>);
-                }
-            };
-
-            return (<div className="row">
-                <div className="col-md-4 col-xs-4"><ViewProfile person={this.state.person} /></div>
-                <div className="col-md-8 col-xs-8">
-                    <form className="form-horizontal">
-                        <div className="form-group">
-                            <div className="col-md-offset-3 col-xs-offset-3 col-md-9 col-xs-9">
-                                <div className="pull-left">
-                                    <button disabled={this.state.history.size < 1} className="btn btn-success btn-block" onClick={this.undo}>Undo</button>
-                                </div>
-                                <div className="pull-right">
-                                    <button disabled={this.state.future.size < 1} className="btn btn-success btn-block" onClick={this.redo}>Redo</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label className="col-md-3 col-xs-3 control-label">First Name:</label>
-                            <div className="col-md-9 col-xs-9">
-                                <input type="text"
-                                       onChange={this.handleChange}
-                                       name="firstName"
-                                       value={this.state.person.get('firstName')}
-                                       className="form-control" placeholder="First Name" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label className="col-md-3 col-xs-3 control-label">Last Name:</label>
-                            <div className="col-md-9 col-xs-9">
-                                <input type="text"
-                                       onChange={this.handleChange}
-                                       name="lastName"
-                                       value={this.state.person.get('lastName')}
-                                       className="form-control" placeholder="Last Name" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label className="col-md-3 col-xs-3 control-label">Country:</label>
-                            <div className="col-md-9 col-xs-9">
-                                <input type="text"
-                                       onChange={this.handleChange}
-                                       name="countryName"
-                                       value={this.state.person.get('country').get('name')}
-                                       className="form-control" placeholder="Country" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label className="col-md-3 col-xs-3 control-label">Friends:</label>
-                            <div className="col-md-9 col-xs-9">
-                                <div className="form-control-static">
-                                    <div className="form-group">
-                                        <div className="col-md-12 col-xs-12">
-                                            <input className="form-control"
-                                                   placeholder="Friend"
-                                                   type="text"
-                                                   onChange={this.handleChange}
-                                                   value={this.state.friend}
-                                                   name="friend"/>
-                                        </div>
-                                    </div>
-                                    <div className="form-group">
-                                        <div className="col-md-9 col-xs-9">
-                                            <button disabled={this.state.friend == ""}
-                                                    className="btn btn-success"
-                                                    onClick={this.addFriend}>Add friend</button>
-                                        </div>
-                                    </div>
-                                    { friendsView(this.state.person.get('friends').toArray()) }
-                                </div>
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <div className="col-md-offset-3 col-xs-offset-3 col-md-9 col-xs-9">
-                                <button className="btn btn-info btn-block" onClick={this.onSave}>Save</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>);
-        }
+        return <View {...this.state}
+            onSave={this.onSave}
+            undo={this.undo}
+            redo={this.redo}
+            handleChange={this.handleChange}
+            addFriend={this.addFriend}
+            removeFriend={this.removeFriend}
+        />
     }
 }
